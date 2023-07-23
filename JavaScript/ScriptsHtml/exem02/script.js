e34b0be26e14d46d1457425b723eb7ff0ef890d6
Silas Matos <silas.progamacao@gmail.com>
2023-07-22 22:05:26 -0300

    22/07

--- JavaScript/ScriptsHtml/exem02/script.js
+++ JavaScript/ScriptsHtml/exem02/script.js
@@ -2,6 +2,6 @@ function  funcao(){
     document.querySelector("div").textContent = "texto novo"
 }
 
-let botao = document.querySelector("button"
+let botao = document.querySelector("button" 
 )  
 botao.addEventListener('click', funcao)
